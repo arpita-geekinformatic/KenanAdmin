4a1081ad82cc4758404a5436712da97075a7cf97
arpita-geekinformatic <arpita@geekinformatic.com>
2022-08-23 10:40:06 +0530

    settings

---
+++ src/app/modules/settings/settings/settings.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-settings',
+  templateUrl: './settings.component.html',
+  styleUrls: ['./settings.component.scss']
+})
+export class SettingsComponent implements OnInit {
+  myForm!: FormGroup;
+
+  ngOnInit() {
+    this.myForm = this.fb.group({
+      name: ['Sammy', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', [Validators.required, Validators.minLength(15)]],
+    });
+  }
+  constructor(private fb: FormBuilder) {}
+
+  onSubmit(form: FormGroup) {
+    console.log('Valid?', form.valid); // true or false
+    console.log('Name', form.value.name);
+    console.log('Email', form.value.email);
+    console.log('Message', form.value.message);
+  }
+
+}
